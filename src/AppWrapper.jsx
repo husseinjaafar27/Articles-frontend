af6d73ab8c0c83080475cb307142e7740126f707
husseinjfr <husseinmohammadjaafar@gmail.com>
2024-02-16 13:01:25 +0200

    .

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,15 +1,17 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthProvider } from "./components/context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AppWrapper = ({ children }) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <BrowserRouter>{children}</BrowserRouter>
+        <BrowserRouter basename="https://articles-frontend-n87r.onrender.com/">
+          {children}
+        </BrowserRouter>
         <ToastContainer />
       </AuthProvider>
     </QueryClientProvider>
